ce95eba56c2410f14b93dbbb2fe90d440eafaab6
王文龙 <357934522@qq.com>
2019-08-19 21:37:02 +0800

    .

--- controller/main.js
+++ controller/main.js
@@ -12,57 +12,57 @@ let task3 = new Task3();
 let task4 = new Task4();
 let task5 = new Task5();
 
-//每小时执行
-schedule.scheduleJob('30 1 * * * *', async function () {
-  try {
-    //更新userSession
-    ganji.mainTask();
-  } catch (err) {
-    console.log(err)
-  }
-  try {
-    //监听精选 是否超过当前设置的预算
-    task4.main();
-  } catch (err) {
-    console.log(err)
-  }
-});
+// //每小时执行
+// schedule.scheduleJob('30 1 * * * *', async function () {
+//   try {
+//     //更新userSession
+//     ganji.mainTask();
+//   } catch (err) {
+//     console.log(err)
+//   }
+//   try {
+//     //监听精选 是否超过当前设置的预算
+//     task4.main();
+//   } catch (err) {
+//     console.log(err)
+//   }
+// });
 
-//每天2点执行
-schedule.scheduleJob('1 1 2 * * *', async function () {
-  try {
-    //映射infoid<-->houseId
-    await task2.main();
-  } catch (err) {
-    console.log(err)
-  }
-  try {
-    //刷新,推送 houseInfo
-    await task1.main();
-  } catch (err) {
-    console.log(err)
-  }
-});
+// //每天2点执行
+// schedule.scheduleJob('1 1 2 * * *', async function () {
+//   try {
+//     //映射infoid<-->houseId
+//     await task2.main();
+//   } catch (err) {
+//     console.log(err)
+//   }
+//   try {
+//     //刷新,推送 houseInfo
+//     await task1.main();
+//   } catch (err) {
+//     console.log(err)
+//   }
+// });
 
-//每天9点执行
-schedule.scheduleJob('1 1 9 * * *', async function () {
-  try {
-    //设置精选
-    await task3.main();
-  } catch (err) {
-    console.log(err)
-  }
-});
+// //每天9点执行
+// schedule.scheduleJob('1 1 9 * * *', async function () {
+//   try {
+//     //设置精选
+//     await task3.main();
+//   } catch (err) {
+//     console.log(err)
+//   }
+// });
 
-//每天19点执行
-schedule.scheduleJob('1 1 19 * * *', async function () {
-  try {
-    //下架精选houseInfo
-    await task5.main();
-  } catch (err) {
-    console.log(err)
-  }
-});
+// //每天19点执行
+// schedule.scheduleJob('1 1 19 * * *', async function () {
+//   try {
+//     //下架精选houseInfo
+//     await task5.main();
+//   } catch (err) {
+//     console.log(err)
+//   }
+// });
 
 
 async function main() {
@@ -71,7 +71,7 @@ async function main() {
     //店铺<-->houseid
     //await task2.main();
     //刷新,推送
-    //await task1.main();
+    await task1.main();
     //精选
     //await task3.main();
     //监听精选
