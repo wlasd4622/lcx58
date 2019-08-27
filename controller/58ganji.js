c9da646a50b74aa0094428f7565b928f31471f25
王文龙 <357934522@qq.com>
2019-08-27 14:03:29 +0800

    .

--- controller/58ganji.js
+++ controller/58ganji.js
@@ -12,7 +12,7 @@ class GanJi {
     this.userList = [];
     this.page = null;
     this.browser = null;
-    this.taskName='58ganji';
+    this.taskName = '58ganji';
     this.dbConfig = {
       host: '101.201.49.69',
       user: 'refresh',
@@ -391,46 +391,48 @@ class GanJi {
     this.log(`>>>yxtgsp58`);
     let result = {};
     //石家庄用户区别
-    if (user.username.includes('石家庄')) {
-      let url = `http://vip.58ganji.com/sydchug/list/sydc`
-      await this.page.goto(url, {
-        waitUntil: 'domcontentloaded'
-      })
-      await this.page.waitForSelector('.platform-data .bold')
-      result = await this.page.evaluate(() => {
-        function foamat() {
-          var data = {}
-          data['还可推送'] = $('.platform-data:eq(0) span:contains(剩余可推广) span').text()
-          data['推送中'] = $('.platform-data:eq(0) span:contains(推广中) span').text()
-          return data;
+    // if (user.username.includes('石家庄')) {
+    let url = `http://vip.58ganji.com/sydchug/list/sydc`
+    await this.page.goto(url, {
+      waitUntil: 'domcontentloaded'
+    })
+    await this.page.waitForSelector('.platform-data .bold')
+    result = await this.page.evaluate(() => {
+      function foamat() {
+        var data = {}
+        data['还可推送'] = $('.platform-data:eq(0) span:contains(剩余可推广) span').text()
+        data['推送中'] = $('.platform-data:eq(0) span:contains(推广中) span').text()
+        return data;
+      }
+      return foamat()
+    })
+    console.log(result);
+    // } else {
+    url = `http://vip.58ganji.com/jp58/yxtgsp58`
+    await this.page.goto(url, {
+      waitUntil: 'domcontentloaded'
+    })
+    await this.page.waitForSelector('.layout-right')
+    let result2 = await this.page.evaluate(() => {
+      function foamat() {
+        var data = {}
+        $('dt:contains(优先推送)').next().find('>span').toArray().map(item => {
+          let info = $(item).text().split('：');
+          data[info[0]] = parseInt(info[1])
+        })
+        let 在线购买 = $('dt:contains(优先推送)').next().find('span:contains(在线购买)').length && $('dt:contains(优先推送)').next().find('span:contains(在线购买)').text().match(/在线购买(\d+)/)[1]
+        if (在线购买) {
+          在线购买 = parseInt(在线购买)
+          data['在线购买'] = 在线购买;
         }
-        return foamat()
-      })
-      console.log(result);
-    } else {
-      let url = `http://vip.58ganji.com/jp58/yxtgsp58`
-      await this.page.goto(url, {
-        waitUntil: 'domcontentloaded'
-      })
-      await this.page.waitForSelector('.layout-right')
-      result = await this.page.evaluate(() => {
-        function foamat() {
-          var data = {}
-          $('dt:contains(优先推送)').next().find('>span').toArray().map(item => {
-            let info = $(item).text().split('：');
-            data[info[0]] = parseInt(info[1])
-          })
-          let 在线购买 = $('dt:contains(优先推送)').next().find('span:contains(在线购买)').length && $('dt:contains(优先推送)').next().find('span:contains(在线购买)').text().match(/在线购买(\d+)/)[1]
-          if (在线购买) {
-            在线购买 = parseInt(在线购买)
-            data['在线购买'] = 在线购买;
-          }
-          return data;
-        }
-        return foamat()
-      })
-    }
-
+        delete data['还可推送'];
+        delete data['推送中'];
+        return data;
+      }
+      return foamat()
+    })
+    // }
+    result={...result2,...result}
     let sql = "insert into `gj_push`(`user_id`,`username`,`in_progress`,`surplus`,`expire`,`purchase`,`create_time`)" +
       "values(" + user.id + ",'" + user.username + "','" + result['推送中'] + "','" + result['还可推送'] + "','" + result['今日推送到期'] + "','" + result['在线购买'] + "',NOW())"
 
